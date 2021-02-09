f4bbc670e17dcb83d56dbde1cef390f697cacedc
NJ dev <30365898+NJdevPro@users.noreply.github.com>
2021-02-09 19:25:10 +0100

    Update demo.c

--- demo.c
+++ demo.c
@@ -9,38 +9,53 @@ int main()
 {
   String* s0 = newString("This string is very long.");
   String* s1 = newString("Shorter string.");
-  printf("[%s] has length %ld and buffer size %ld\n", s0->str, s0->len, s0->sz);
-  printf("[%s] has length %ld and buffer size %ld\n", s1->str, s1->len, s1->sz);
+  printf("s0 = [%s] has length %ld and buffer size %ld\n", s0->str, s0->len, s0->sz);
+  printf("s1 = [%s] has length %ld and buffer size %ld\n", s1->str, s1->len, s1->sz);
   
   stringcat(s0, s1);
-  printf("After concatenation, ");
-  printf("[%s] has length %ld and buffer size %ld\n", s0->str, s0->len, s0->sz);
-  
-  String *s2 = stringdup(s1);
-  printf("Duplicate of [%s] has length %ld and buffer size %ld\n", s2->str, s2->len, s2->sz);
-  
+  printf("After concatenation of s1 into s0, ");
+  printf("s0 = [%s] has length %ld and buffer size %ld\n", s0->str, s0->len, s0->sz);
+    
   stringtrunc(s0, 12);
-  printf("[%s] has length %ld and buffer size %ld\n", s0->str, s0->len, s0->sz);
+  printf("Truncated s0 = [%s] has length %ld and buffer size %ld\n", s0->str, s0->len, s0->sz);
+    
+  String *s2 = stringdup(s1);
+  printf("Duplicate s2 = [%s] of s1 has length %ld and buffer size %ld\n", s2->str, s2->len, s2->sz);
+  
+  stringchcat(s0, ".C null terminated string.");
+  printf("After concatenation of a C string, s0 = [%s] has length %ld and buffer size %ld\n", s0->str, s0->len, s0->sz);
+    
+  stringcpy(s2, s0);
+  printf("Copy of s0, s2 = [%s] has length %ld and buffer size %ld\n\n", s2->str, s2->len, s2->sz);
+    
   delString(s0);
   delString(s1);
   delString(s2);
 
   // Allocation on the stack
+  // ========================
   static char t0[50] = "This LString is very long.";
   static char t1[30] = "Shorter LString.";
-  LString ls0 = localString(t0, 50);
-  LString ls1 = localString(t1, 30);
+  static char t2[40] = "";
+  LString l0 = localString(t0, 50);
+  LString l1 = localString(t1, 30);
+  printf("l0 = [%s] has length %ld and buffer size %ld\n", l0.str, l0.len, l0.sz);
+  printf("l1 = [%s] has length %ld and buffer size %ld\n", l1.str, l1.len, l1.sz);
   
-  lstringcat(&ls1, &ls0);
+  lstringcat(&l1, &l0);
+  printf("After concatenation of l1 into l0, ");
+  printf("l0 = [%s] has length %ld and buffer size %ld\n", l0.str, l0.len, l0.sz);
   
-  printf("[%s] has length %ld\n", ls0.str, ls0.len);
-  printf("[%s] has length %ld\n", ls1.str, ls1.len);
+  stringtrunc((String*)&l0, 13);
+  printf("Truncated l0 = [%s] has length %ld and buffer size %ld\n", l0.str, l0.len, l0.sz);
   
-  LString ls2 = lstringdup(&ls0);
-  printf("[%s] has length %ld and buffer size %ld\n", ls2.str, ls2.len, ls2.sz);
-  lstringchcat(&ls2, " This is a C chain.");
-  printf("[%s] has length %ld and buffer size %ld\n", ls2.str, ls2.len, ls2.sz);
+  LString l2 = localString(t2, 40); // remember that localString("", 40) does NOT work !
+  lstringcpy(&l2, &l0);
+  printf("Copy of l0, l2 = [%s] has length %ld and buffer size %ld\n", l2.str, l2.len, l2.sz);
   
-  printf(stringchcmp((String*) &ls1, t1) == 0 ? "The chains are identical" : "The chains are different");
+  lstringchcat(&l0, ". This is a C chain.");
+  printf("After concatenation of a C chain, l0 = [%s] has length %ld and buffer size %ld\n", l0.str, l0.len, l0.sz);
+  
+  printf(stringchcmp((String*)&l1, t1) == 0 ? "The chains are identical" : "The chains are different");
   return 0;
 }
